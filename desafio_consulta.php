427b2462a1390947dc97b4a24bbb3dae3120fe7b
DarlanLS2 <darlanleite.arnaldo@gmail.com>
2024-09-30 20:52:27 -0300

    adicionei os desafios

---
+++ desafio_consulta.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <center>
+    <h1> Consulta de Carros </h1>
+    <form action="exibir_consulta.php" method="POST">
+      <label for="id">Insira o ID do carro:</label><br>
+      <input type="text" name="id"/><br>
+      <input type="submit" value="consultar"/>
+    </form>
+  </center>
+  <br><br><br>
+</body>
+</html>
